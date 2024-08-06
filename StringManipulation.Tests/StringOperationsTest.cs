c3147b2803c775a1923c713c43335c8e490ac31b
Alex Coronell <alexcoronell@gmail.com>
2024-08-05 19:28:07 -0500

    First commit, ConcatenateStrings test complete

---
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation.Tests
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperarrions = new StringOperations();
+
+            var result = strOperarrions.ConcatenateStrings("Hello", "Platzi");
+            Console.WriteLine(result);
+            Assert.Equal("Hello Platzi", result);
+        }
+    }
+}
